2e59420b5150d5f09888d748223bf4e101de8a6f
peter <bearkingbrother@gmail.com>
2017-08-15 17:16:29 +0800

    v1.0 b5 (changed bundleId)

--- app/src/main/java/com/rafflerchat/app/country/CountryPickerListener.java
+++
@@ -1,9 +0,0 @@
-package com.rafflerchat.app.country;
-
-/**
- * Created by Ghost on 14/8/2017.
- */
-
-public interface CountryPickerListener {
-    public void onSelectCountry(String name, String code, String dialCode, int flagDrawableResID);
-}
